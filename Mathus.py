ddd33a733270648ab7c70820b9de7425b557bdbb
hexzedels <hexpisos@gmail.com>
2020-02-16 21:17:05 +0300

    Minor fix

--- Mathus.py
+++ Mathus.py
@@ -7,9 +7,9 @@ pd.options.mode.chained_assignment = None
 data = pd.read_csv('data1.csv')
 theme_completer = WordCompleter(np.load('Titles.npy'))
 inp = prompt('Enter theme: ', completer=theme_completer)
+#inp = prompt('Enter theme: ', completer=theme_completer)#Uncomment if 
 work_frame = data[data['Title'] == inp]
 indx = work_frame.index[0]
-print('Total amount of problems: ',data['Total'][indx])
 def fix_problems(frame): #Some crazy fix
     app =frame['Solved'][indx][1:-1].split(" ")
     temp = []
@@ -24,8 +24,7 @@ def add(problems):
     data['Solved'][indx] = np.array(temp)
     return temp 
 temp = fix_problems(work_frame)
-print('Solved')
-print('Currently solved: ',sum(1 for i in temp if i  == 1))
+print('Solved ',sum(1 for i in temp if i  == 1),' of ',data['Total'][indx])
 print(temp)
 add(input("Enter problems separated by comma: "))
 data.to_csv('data1.csv',index=False)
